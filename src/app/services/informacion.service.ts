46860f3825b8e9e647a32939c414c632d0846e3e
Oscar luna <oscarlhdz93@gmail.com>
2018-06-03 21:14:44 -0500

    servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,14 +4,29 @@ import { Http } from '@angular/http';
 export class InformacionService {
   
   info:any = {};
-  datacargada:boolean = false;
+  cargada:boolean = false;
+  cargada_sobre_nosotros:boolean =  false;
+  equipo:any[] = [];
 
   constructor( public http:Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+  
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json").
-      subscribe( data => {
-        console.log(data.json());
-        this.info = data.json();
-        this.datacargada = true;
-      })
+    subscribe( data => {
+      this.info = data.json();
+      this.cargada = true;
+    })
    }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://pagina-portafolio.firebaseio.com/equipo.json").
+    subscribe( data => {
+      this.equipo = data.json();
+      this.cargada_sobre_nosotros = true;
+      console.log(this.equipo);
+    })
+  }
 }
