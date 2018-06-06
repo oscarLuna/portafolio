49412b6a7c6478f28766b34992773de276b58a99
Oscar luna <oscarlhdz93@gmail.com>
2018-06-05 22:59:51 -0500

    primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,21 +6,50 @@ import { Http } from '@angular/http';
 })
 export class ProductosService {
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando_productos:boolean = true;
 
   constructor(private http:Http) {
     this.cargar_productos();
   }
 
+  public buscar_producto(termino:string){
+    console.log(this.productos.length);
+
+    if(this.productos.length === 0){
+      this.cargar_productos().then( ()=>{
+        this.filtrar_producto(termino);
+      });
+    } else {
+      this.filtrar_producto(termino);
+    }
+  }
+
+  private filtrar_producto(termino:string){
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+    this.productos.forEach(prod => {
+      console.log(prod);
+      if(prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0){
+        this.productos_filtrado.push(prod);
+      }
+    })
+  }
+
   public cargar_productos(){
     this.cargando_productos = true;
-    this.http.get('https://pagina-portafolio.firebaseio.com/productos_idx.json')
+
+    let promesa = new Promise( ( resolve, reject ) => {
+      this.http.get('https://pagina-portafolio.firebaseio.com/productos_idx.json')
       .subscribe( res => {
-        setTimeout( ()=>{
+        //setTimeout( ()=>{
           this.cargando_productos = false;
           this.productos=res.json();
-        },1500)
+          resolve();
+        //},1500)
       });
+    });
+    return promesa;
   }
   
   public cargar_producto(cod:string){
